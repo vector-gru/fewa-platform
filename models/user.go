38eb91b2e8838bd516882c5a206f1fb1870be953
Nsah <akutala@gmail.com>
2024-07-17 13:46:22 +0100

    the registration works

--- models/user.go
+++ models/user.go
@@ -8,19 +8,7 @@ type User struct {
     LastName             string    `json:"last_name"`
     Email                string    `json:"email" gorm:"uniqueIndex"`
     Password             string    `json:"password"`
-    PhoneNumber          string    `json:"phone_number"`
-    DateOfBirth          time.Time `json:"date_of_birth"`
-    Gender               string    `json:"gender"`
-    ProfilePicture       string    `json:"profile_picture"`
-    StreetAddress        string    `json:"street_address"`
-    City                 string    `json:"city"`
-    State                string    `json:"state"`
-    PostalCode           string    `json:"postal_code"`
-    Country              string    `json:"country"`
-    PreferredLanguage    string    `json:"preferred_language"`
-    TimeZone             string    `json:"time_zone"`
-    AgreeToTerms         bool      `json:"agree_to_terms"`
-    SubscribeToNewsletter bool      `json:"subscribe_to_newsletter"`
+    AgreeToTerms         string    `json:"agree_to_terms"`  // Changed to string
     CreatedAt            time.Time `json:"created_at"`
     UpdatedAt            time.Time `json:"updated_at"`
 }
